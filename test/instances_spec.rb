876c560d0c4f262bf38064443cc4ff304588c252
tamy0612 <fakey.insanity@gmail.com>
2014-04-22 17:01:16 +0900

    fixed

--- test/instances_spec.rb
+++ test/instances_spec.rb
@@ -9,14 +9,6 @@ describe "instances_checker" do
     @optimum = JSON.parse( open("#{@pwd}/test/bestknowns.json").readlines.map(&:chomp).join )
   end
 
-  it 'verifies optimum makespan' do
-    @instances.each do |instance|
-      unless instance["optimum"].nil?
-        instance["optimum"].should eq(@optimum[instance["name"]])
-      end
-    end
-  end
-
   it 'verifies instance file path' do
     @instances.each do |instance|
       File.exist?( "#{@pwd}/#{instance["path"]}" ).should be_true
@@ -27,6 +19,12 @@ describe "instances_checker" do
     @instances.each do |instance|
       size = open( "#{@pwd}/#{instance["path"]}" ).readlines.reject{|line| line =~ /^#/}[0].split.map(&:to_i)
       [instance["jobs"],instance["machines"]].should eq(size)
+    end
+  end
+
+  it 'verifies bounds' do
+    @instances.select{|instance| instance["optimum"].nil? }.each do |instance|
+      instance["bounds"].tap{|b| break ( b.nil? || b["upper"] > b["lower"] ) }.should be_true
     end
   end
 
