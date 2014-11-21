eea2b60dd7e2f5c907ff7302662c61812eb7efdf
tamy0612 <fakey.insanity@gmail.com>
2014-11-21 18:44:49 +0900

    refine loader

--- script/load_instance.rb
+++ script/load_instance.rb
@@ -1,19 +1,52 @@
 require 'json'
 
-Target = ARGV.first
-
 Root = "#{File.dirname(__FILE__)}/../"
 
-file = open("#{Root}/instances.json")
-json = JSON.parse( file.readlines.map(&:chomp).join )
+class Instance
+  def initialize hash
+    @hash = hash
+  end
+  def name
+    @hash["name"]
+  end
+  def jobs
+    @hash["jobs"]
+  end
+  def machines
+    @hash["machines"]
+  end
+  def operations
+    self.jobs * self.machines
+  end
+  def path
+    @hash["path"]
+  end
+  def optimum
+    opt = @hash["optimum"]
+    bnd = @hash["bounds"]
+    return "nan" if [opt,bnd].compact.empty?
+    return opt unless opt.nil?
+    bnd["lower"]
+  end
+  def bounds
+    @hash["bounds"]
+  end
+end
 
-instance = json.select{|data| data["name"] == Target }
-raise "There is no instance named '#{target}'" if instance.empty?
 
-instance = instance.first
-path = File.expand_path( instance["path"], Root )
-optimum = instance["optimum"]
-optimum ||= instance["bounds"]["lower"] unless instance["bounds"].nil?
-optimum ||= "nan"
+def load_instance target, dir = Root
+  file = open("#{Root}/instances.json")
+  json = JSON.parse( file.readlines.map(&:chomp).join )
+  instance = json.select{|data| data["name"] == target }
+  raise "There is no instance named '#{target}'" if instance.empty?
+  instance = instance.first
+  instance["path"] = File.expand_path( instance["path"], dir )
+  Instance.new( instance )
+end
 
-print "--instance %s --optimum %s" % [path,optimum]
+if $0 == __FILE__
+  target,dir = ARGV
+  dir ||= Root
+  instance = load_instance( target, dir )
+  print "--instance %s --optimum %s" % [instance.path,instance.optimum]
+end
